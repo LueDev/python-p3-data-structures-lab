eff82363d281b1b645f77c2e7ba6f18fbe301eb9
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-01-25 23:53:05 -0500

    One and done! Great lecture and assignment to better understand lambda funcs, list comprehensions, reduce, list methods, and print functions better. Very excited for the next. All tests passing.

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -17,22 +20,39 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [obj['name'] for obj in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [obj for obj in spicy_foods if obj['heat_level'] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    ## Joining with string
+    ## list1 = " geeks "
+    ## print("$".join(list1))
+    # PRINTS $g$e$e$k$s$ so joining strings with "\n" joins them through newlines.
+    ## Without the join to treat this output like a string, this creates a generator func which we don't want. 
+    print("\n".join((f"{obj['name']} ({obj['cuisine']}) | Heat Level: {'🌶' * obj['heat_level']}") for obj in spicy_foods))
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    return [obj for obj in spicy_foods if obj['cuisine'] == cuisine.title()][0]
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    print("\n".join([f"{obj['name']} ({obj['cuisine']}) | Heat Level: {'🌶' * obj['heat_level']}" for obj in spicy_foods if obj['heat_level'] > 5]))
 
 def get_average_heat_level(spicy_foods):
-    pass
+    ## functools.reduce(function, iterable, initial)
+    ## x is assigned the initial value 0 while y is the current element in target list/array 
+    ## Therefore, 0 + 9 occurs. Since x is the accumulator, it carries the previous result into
+    ## the next iteration of reduce so 9 + 3 occurs.
+    return int((reduce(lambda x,y: x + y['heat_level'], spicy_foods, 0)) / len(spicy_foods))
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
+
+print(create_spicy_food(spicy_foods,{
+        'name': 'Griot',
+        'cuisine': 'Haitian',
+        'heat_level': 10,
+    } ))
